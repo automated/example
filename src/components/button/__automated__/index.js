10edc5960715d9de7244153f0eab156ef1d0252d
Kirk Strobeck <kirk@strobeck.com>
2017-11-12 17:11:14 -0800

    Moving closer to default

--- src/components/button/__automated__/index.js
+++ src/components/button/__automated__/index.js
@@ -1,9 +1,5 @@
 import BaseComponent from '../';
 
-// export const title = 'Button';
-
-export const dirpath = '/src/components/button';
-
 export const Component = BaseComponent;
 
 export const useCases = [
